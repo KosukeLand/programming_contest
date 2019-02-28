841fb52777a1c9de89889e48afd271e4a216e0dc
Kosuke <kosuke@example.com>
2019-02-28 23:39:50 +0900

    Refactoring

--- AtCoder_Beginner_Contest_119/C_Synthetic_Kadomatsu.js
+++ AtCoder_Beginner_Contest_119/C_Synthetic_Kadomatsu.js
@@ -1,7 +1,7 @@
 var lines = [];
 
 var N, A, B, C;
-var l, f
+var l;
 var result = Infinity;
 
 var readline = require("readline");
@@ -24,8 +24,6 @@ rl.on('close', function () {
   C = Number(tmp.split(" ")[3]);
 
   l = Array(N);
-  f = Array(N);
-  f.fill(Infinity);
 
   for (var i = 0; i < N; i++) { l[i] = Number(lines[i]); }
 
@@ -38,26 +36,14 @@ function dfs(n, a, b, c) {
   if (n === N) {
     if (a === 0 || b === 0 || c === 0) { return (Infinity); }
     else {
-      return(Math.abs(a - A) + Math.abs(b - B) + Math.abs(c - C));
+      return (Math.abs(a - A) + Math.abs(b - B) + Math.abs(c - C));
     }
   }
-  if(f[n]!==Infinity){
-  //   console.log("f[n]",n,f[n]);    
-  }
+
   var res_a = dfs(n + 1, a + l[n], b, c) + (a != 0 ? 10 : 0);    // l[i]をAに加える
-  // console.log("res_a",n,a,b,c,res_a);    
-  
   var res_b = dfs(n + 1, a, b + l[n], c) + (b != 0 ? 10 : 0);    // l[i]をBに加える
-  // console.log("res_b",n,a,b,c,res_b);    
-  
   var res_c = dfs(n + 1, a, b, c + l[n]) + (c != 0 ? 10 : 0);    // l[i]をCに加える
-  // console.log("res_c",n,a,b,c,res_c); 
-
   var res_x = dfs(n + 1, a, b, c);                               // l[i]加えない
-  // console.log("res_x",n,a,b,c,res_x); 
 
-
-  f[n] = Math.min(res_a, res_b, res_c, res_x);
-  // console.log(res_a, res_b, res_c, res_x,f[n]);
-  return (f[n]);
+  return (Math.min(res_a, res_b, res_c, res_x));
 }
