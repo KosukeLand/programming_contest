7a670ef54d38fbfaaab796f07c84679d30f6a04e
Kosuke <kosuke@example.com>
2019-03-28 21:17:08 +0900

    解説見てなんとか理解したけど，自力で解くなんて無理

--- AtCoder_Beginner_Contest_104/D_We_Love_ABC.js
+++ AtCoder_Beginner_Contest_104/D_We_Love_ABC.js
@@ -1,6 +1,5 @@
-var line; var result = 0;
-var S; var abc = ["A", "B", "C"];
-var MOD = Math.pow(10,9)+7;
+var line; var S;
+var MOD = Math.pow(10, 9) + 7;
 
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -14,54 +13,23 @@ rl.on('line', function (x) {
 
 rl.on('close', function () {
     S = line.split("");
-    dfs(0, []);
-    console.log(result);
-});
-
-function dfs(n, comb) {
-    if (n === S.length) { calc(comb); return (0); }
-
-    if (S[n] === "?") {
-        comb[n] = "A"; dfs(n + 1, comb);
-        comb[n] = "B"; dfs(n + 1, comb);
-        comb[n] = "C"; dfs(n + 1, comb);
-    }
-    else {
-        comb[n] = S[n]; dfs(n + 1, comb);
-    }
-}
-
-function calc(comb) {
-
-    var dp = Array(S.length).fill(0)
-    for (var i = 0; i < S.length; i++) {
-        dp[i] = Array(3).fill(0);
+    dp = Array(S.length + 1);
+    
+    for (var i = 0; i <= S.length; i++) {
+        dp[i] = Array(3 + 1).fill(0);
     }
 
-    if (comb[0] === abc[0]) { dp[0][0] += 1; }
-
-    for (var i = 1; i < S.length; i++) {
-        if (comb[i] === abc[0]) { dp[i][0] += dp[i - 1][0] + 1; }
-        else { dp[i][0] = dp[i - 1][0]; }
-    }
-
-    for (var i = 1; i < abc.length; i++) {
-        dp[0][i] = 0;
-    }
-
-    for (var j = 1; j < 3; j++) {
-        var abc_counter = {};
-        for (var i = 1; i < S.length; i++) {
-            if (abc_counter[comb[i]] === undefined) abc_counter[comb[i]] = 0;
-
-            if (comb[i] === abc[j] && dp[i - 1][j - 1] !== 0) {
-                dp[i][j] += dp[i - 1][j - 1] + abc_counter[comb[i]];
-                abc_counter[comb[i]]++;
-            }
+    for (var i = S.length; i >= 0; i--) {
+        for (var j = 3; j >= 0; j--) {
+            if (i === S.length) { dp[i][j] = (j === 3 ? 1 : 0); }
             else {
-                dp[i][j] = dp[i - 1][j];
+                dp[i][j] = dp[i + 1][j] * (S[i] === '?' ? 3 : 1);
+                if (j < 3 && (S[i] === '?' || S[i] === "ABC"[j])) {
+                    dp[i][j] += dp[i + 1][j + 1];
+                }
+                dp[i][j] %= MOD;
             }
         }
     }
-    result += (dp[S.length - 1][2] % MOD);
-}
+    console.log(dp[0][0]);
+});
