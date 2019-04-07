7b7215356d655a5ca8cf2955ec977d16a7b29871
Kosuke <kosuke@example.com>
2019-04-07 13:34:47 +0900

    AC

--- AtCoder_Beginner_Contest_123/C_Five_Transportations.js
+++ AtCoder_Beginner_Contest_123/C_Five_Transportations.js
@@ -19,35 +19,12 @@ rl.on('close', function () {
         v[i] = Number(lines[i]);
     }
 
-    var a = Array(6).fill(0);
     var result = 0;
+    var min = Infinity; var j = 0;
 
-    a[0] = N;
-    console.log(a);
-    
-    while (1) {
-        if (v[4] < a[4]) { a[4] -= v[4]; a[5] += v[4]; }
-        else if (v[4] >= a[4] && a[4] !== 0) { a[5] += a[4]; a[4] = 0; }
-        else { }
+    for (var i = 0; i < 5; i++) { if (v[i] < min) { min = v[i]; j = i; } }
 
-        if (v[3] < a[3]) { a[3] -= v[3]; a[4] += v[3]; }
-        else if (v[3] >= a[3] && a[3] !== 0) { a[4] += a[3]; a[3] = 0; }
-        else { }
-
-        if (v[2] < a[2]) { a[2] -= v[2]; a[3] += v[2]; }
-        else if (v[2] >= a[2] && a[2] !== 0) { a[3] += a[2]; a[2] = 0; }
-        else { }
-
-        if (v[1] < a[1]) { a[1] -= v[1]; a[2] += v[1]; }
-        else if (v[1] >= a[1] && a[1] !== 0) { a[2] += a[1]; a[1] = 0; }
-        else { }
-
-        if (v[0] < a[0]) { a[0] -= v[0]; a[1] += v[0]; }
-        else if (v[0] >= a[0] && a[0] !== 0) { a[1] += a[0]; a[0] = 0; }
-        else { }
-
-        result++;
-        if (a[5] === N) { break; }
-    }
+    result = Math.ceil(N / v[j]) + 4;
     console.log(result);
+
 });
