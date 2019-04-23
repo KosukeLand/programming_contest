911c48ced4d878fc082f395a0f00e0d473711a2c
Kosuke <kosuke@example.com>
2019-04-23 21:28:06 +0900

    解き直し

--- AtCoder_Beginner_Contest_116/C_Grand_Garden.js
+++ AtCoder_Beginner_Contest_116/C_Grand_Garden.js
@@ -1,36 +1,33 @@
-var lines =[];
+var lines = []; var result = 0;
+
 var readline = require("readline");
+
 var rl = readline.createInterface({
-  input:  process.stdin,
+  input: process.stdin,
   output: process.stdout
 });
 
-rl.on('line',function(x){
+rl.on('line', function (x) {
   lines.push(x);
 });
 
-rl.on('close',function(){
-  var n = Number(lines[0]);
-  var h = lines[1].split(" ");
-  var ite = 0;
-  var flag = false;
+rl.on('close', function () {
+  var N = Number(lines.shift());
+  var h = lines.shift().split(" ").map(value => Number(value));
 
-  var zero= Array(h.length);
-  zero.fill(0);
+  // 100*100程度なので，全探索可能
+  for (var i = 0; i < 100; i++) {
+    var zero = true;
 
-  while(h.toString() !== zero.toString()){
-    for(var i=0;i<h.length;i++){
-      if(Number(h[i])>0){
-        flag = true;
-        h[i] = Number(h[i]) -1;
-      }
-      else{
-        if(flag === true){ite++;}
-        flag = false;
-      }
+    for (var j = 0; j < N; j++) {
+      // 120みたいにh[j]以前に0がなければ，result++
+      if (h[j] === 0 && zero === false) { result++; zero = true; }
+      // h[j]がよりも大きかったら，1cm縮む
+      if (1 <= h[j]) { h[j]--; zero = false }
+
     }
-    if(flag === true){ite++;}
-    flag = false;
+    // ..234のように，配列の終わりにh[j]===0が出現しない場合，result++してあげる
+    if (0 <= h[N - 1] && zero === false) { result++; }
   }
-  console.log(ite);
+  console.log(result);
 });
